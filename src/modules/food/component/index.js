192029fe6aa9b0a2bafaf738e83efc8c4d12fb34
weiwang <2424947384@qq.com>
2019-01-09 22:29:34 +0800

    修改长列表，对接接口，未完待续

--- src/modules/food/component/index.js
+++ src/modules/food/component/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import {NavBar, Carousel, Tabs, Toast, SearchBar} from 'antd-mobile';
+import {ListView, Tabs, Toast, SearchBar} from 'antd-mobile';
 import '../index.less';
 import img1 from 'Img/1.jpg'
 import img from 'Img/IMG_1624.png'
 import DocumentTitle from "react-document-title";
 import axios from "Utils/axios";
+import assign from 'lodash/assign';
 
 const GoodsCart = ({className = '', data, ...restProps}) => (
   <div className={`${className} goodsCart`} {...restProps}>
@@ -18,7 +20,7 @@ const GoodsCart = ({className = '', data, ...restProps}) => (
         {data.title}
       </div>
       <div className='goodsContent'>
-        <div className='goodsLabel'>【{data.name}】</div>
+        <div className='goodsLabel'>【{data.productName}】</div>
         <div className='goodsAddress'>{data.address}</div>
       </div>
       <div className='goodsFooter'>
@@ -28,252 +30,194 @@ const GoodsCart = ({className = '', data, ...restProps}) => (
   </div>
 );
 
+function RenderContent(props) {
+  return (<div className='tab-body am-list-body'>
+    {props.children}
+  </div>);
+}
+
+let pageIndex = 1;
+const dataBlobs = {};
+let sectionIDs = [];
+let rowIDs = [];
+
+function genData(pIndex = 0) {
+
+  const sectionName = `Section ${pIndex}`;
+  sectionIDs.push(sectionName);
+  dataBlobs[sectionName] = sectionName;
+  rowIDs[pIndex] = [];
+
+  const rowName = `S${pageIndex}, R${pageIndex}`;
+  rowIDs[pIndex].push(rowName);
+  dataBlobs[rowName] = rowName;
+
+  sectionIDs = [...sectionIDs];
+  rowIDs = [...rowIDs];
+}
 
 class Index extends React.Component {
   constructor(props) {
     super(props);
+    const getSectionData = (dataBlob, sectionID) => dataBlob[sectionID];
+    const getRowData = (dataBlob, sectionID, rowID) => dataBlob[rowID];
+
+    const dataSource = new ListView.DataSource({
+      getRowData,
+      getSectionHeaderData: getSectionData,
+      rowHasChanged: (row1, row2) => row1 !== row2,
+      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+    });
 
     this.state = {
-      data: ['1', '2', '3'],
-      category: [
-        {
-          title: '全部',
-          id: 1,
-          goodsList: [
-            {
-              id: 1,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 200,
-              isTop: true
-            },
-            {
-              id: 2,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 300,
-              isTop: false
-            },
-            {
-              id: 3,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 400,
-              isTop: true
-            },
-            {
-              id: 4,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 500,
-              isTop: false
-            }]
-        }, {
-          title: '酒水',
-          id: 2,
-          goodsList: [
-            {
-              id: 1,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 200,
-              isTop: true
-            },
-            {
-              id: 2,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 300,
-              isTop: false
-            },
-            {
-              id: 3,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 400,
-              isTop: true
-            },
-            {
-              id: 4,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 500,
-              isTop: false
-            }]
-        },
-        {
-          title: '肉干零嘴',
-          id: 3,
-          goodsList: [
-            {
-              id: 1,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 200,
-              isTop: true
-            },
-            {
-              id: 2,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 300,
-              isTop: false
-            },
-            {
-              id: 3,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 400,
-              isTop: true
-            },
-            {
-              id: 4,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 500,
-              isTop: false
-            }, {
-              id: 5,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 400,
-              isTop: true
-            },
-            {
-              id: 6,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 500,
-              isTop: false
-            }]
-        },
-        {
-          title: '特色糕点',
-          id: 4,
-          goodsList: [
-            {
-              id: 1,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 200,
-              isTop: true
-            },
-            {
-              id: 2,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 300,
-              isTop: false
-            },
-            {
-              id: 3,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 400,
-              isTop: true
-            },
-            {
-              id: 4,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 500,
-              isTop: false
-            }]
-        },
-        {
-          title: '特色主食',
-          id: 5,
-          goodsList: [
-            {
-              id: 1,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 200,
-              isTop: true
-            },
-            {
-              id: 2,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 300,
-              isTop: false
-            },
-            {
-              id: 3,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 400,
-              isTop: true
-            },
-            {
-              id: 4,
-              name: '太平鸟',
-              title: '太平鸟那幢冬季短款黑色羽绒服',
-              address: '杭州',
-              price: 500,
-              isTop: false
-            }]
-        }
-      ],
+      isLoading: false,
+      dataSource,
+      height: document.documentElement.clientHeight * 3 / 4,
+      params: {
+        pageNumber: 1,
+        pageSize: 10,
+      },
+      keyWords: '',
+      categoryList: [{
+        title: '全部'
+      }],
+      goodsList: [
+        // {
+        //   id: 1,
+        //   name: '太平鸟',
+        //   title: '太平鸟那幢冬季短款黑色羽绒服',
+        //   address: '杭州',
+        //   price: 200,
+        //   isTop: true
+        // },
+        // {
+        //   id: 2,
+        //   name: '太平鸟',
+        //   title: '太平鸟那幢冬季短款黑色羽绒服',
+        //   address: '杭州',
+        //   price: 300,
+        //   isTop: false
+        // },
+        // {
+        //   id: 3,
+        //   name: '太平鸟',
+        //   title: '太平鸟那幢冬季短款黑色羽绒服',
+        //   address: '杭州',
+        //   price: 400,
+        //   isTop: true
+        // },
+        // {
+        //   id: 4,
+        //   name: '太平鸟',
+        //   title: '太平鸟那幢冬季短款黑色羽绒服',
+        //   address: '杭州',
+        //   price: 500,
+        //   isTop: false
+        // }
+      ]
     }
   };
 
   componentWillMount() {
-    this.queryCategory()
+    this.queryFoodsCategory();
+    this.queryFoods();
   }
 
   componentDidMount() {
+    const hei = document.documentElement.clientHeight - ReactDOM.findDOMNode(this.lv).parentNode.offsetTop;
+    // simulate initial Ajax
+    setTimeout(() => {
+      genData();
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlobs, sectionIDs),
+        isLoading: false,
+        height: hei,
+      });
+    }, 600);
   }
 
-  queryCategory = () => {
+  queryFoodsCategory = () => {
+    const {categoryList} = this.state
     axios.get('product/queryAllCategoryList')
       .then(res => res.data).then(data => {
       if (data.success) {
-
+        let list = data.backData.map(item => {
+          return {
+            ...item,
+            title: item.productCategoryName
+          }
+        })
+        this.setState({
+          categoryList: categoryList.concat(list)
+        })
       } else {
-        Toast.fail('查询列表失败');
+        Toast.fail(data.backMsg);
+      }
+    });
+  }
+
+  queryFoods = () => {
+    const {params, keyWords} = this.state;
+    const param = assign({}, params, {keyWords});
+    axios.get('product/queryList', {
+      params: param
+    }).then(res => res.data).then(data => {
+      if (data.success && data.backData && data.backData.content) {
+        this.setState({
+          goodsList: data.backData.content
+        })
+      } else {
+        Toast.fail(data.backMsg);
       }
       this.setState({initLoading: false});
     });
   }
+
+  tabChange = (tab, index) => {
+    // console.log(tab, index)
+  }
+
   detail = (id) => {
     this.context.router.push(`/food/detail/${id}`);
   }
 
-  renderContent = tab =>
-    (<div className='tab-body'>
-      {
-        tab.goodsList.map(item => {
-          return (
-            <GoodsCart key={item.id} data={item} onClick={() => this.detail(item.id)}/>
-          )
-        })
+  onEndReached = (event) => {
+    // load new data
+    // hasMore: from backend data, indicates whether it is the last page, here is false
+    if (this.state.isLoading && !this.state.hasMore) {
+      return;
+    }
+
+    this.setState({
+      isLoading: true,
+      params: {
+        pageSize:10,
+        pageNumber: this.state.params.pageNumber
       }
-    </div>);
+    });
+    setTimeout(() => {
+      genData(this.state.params.pageNumber);
+      this.queryFoods();
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlobs, sectionIDs, rowIDs),
+        isLoading: false,
+      });
+    }, 1000);
+  }
 
   render() {
-    const {goodsList, category} = this.state;
+    const {dataSource, categoryList, height, params, goodsList, isLoading} = this.state;
+    const row = (rowData, sectionID, rowID) => {
+      return (
+        <div key={rowID} style={{'display': 'flex', 'flexWrap':'wrap'}}>
+          {
+            goodsList.map((item) => (
+              <GoodsCart key={item.id} data={item} onClick={() => this.detail(item.id)}/>
+            ))
+          }
+        </div>
+      );
+    }
 
     return (
       <DocumentTitle title='特色食品'>
@@ -281,10 +225,28 @@ class Index extends React.Component {
           <div className="zui-content">
             <SearchBar placeholder="请输入要搜索的商品" maxLength={16}/>
             <Tabs
-              tabs={category}
-              swipeable={true}
-              renderTabBar={props => <Tabs.DefaultTabBar {...props} page={6}/>}>
-              {this.renderContent}
+              tabs={categoryList}
+              swipeable={false}
+              onChange={this.tabChange}
+              renderTabBar={props => <Tabs.DefaultTabBar {...props} page={6}/>}
+            >
+              <ListView
+                ref={el => this.lv = el}
+                dataSource={dataSource}
+                renderFooter={() => (<div style={{padding: 30, textAlign: 'center'}}>
+                  {isLoading ? '正在加载...' : '加载完成'}
+                </div>)}
+                renderBodyComponent={() => <RenderContent/>}
+                renderRow={row}
+                style={{
+                  height: height,
+                  overflow: 'auto',
+                }}
+                pageSize={1}
+                scrollRenderAheadDistance={500}
+                onEndReached={this.onEndReached}
+                onEndReachedThreshold={10}
+              />
             </Tabs>
           </div>
         </div>
