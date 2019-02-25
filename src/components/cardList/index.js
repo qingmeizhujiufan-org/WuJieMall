dabd823e60c4e9414b5c683e91166087f5630737
weiwang <2424947384@qq.com>
2019-02-25 22:32:36 +0800

    修改商品详情页，添加商铺详情页

--- src/components/cardList/index.js
+++ src/components/cardList/index.js
@@ -7,274 +7,274 @@ import axios from 'axios';
 import './index.less';
 
 function MyBody(props) {
-    return (
-        <div className='zui-cardlist-body'>
-            {props.children}
-        </div>
-    );
+  return (
+    <div className='zui-cardlist-body'>
+      {props.children}
+    </div>
+  );
 }
 
 class Index extends React.Component {
-    constructor(props) {
-        super(props);
-        let dataSource = new ListView.DataSource({
-            rowHasChanged: (row1, row2) => row1 !== row2,
-        });
+  constructor(props) {
+    super(props);
+    let dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2,
+    });
 
-        this.state = {
-            dataSource,
-            initLoaded: false,
+    this.state = {
+      dataSource,
+      initLoaded: false,
+      refreshing: false,
+      isLoading: false,
+      listData: [],
+      pageIndex: 0,
+      params: {},
+      height: document.documentElement.clientHeight || document.body.clientHeight
+    };
+  }
+
+  componentWillMount() {
+    this.setState({
+      params: this.props.params
+    });
+
+    window.addEventListener('resize', this.settingHeight);
+  }
+
+  componentDidMount() {
+    const {dataSource} = this.state;
+    this.settingHeight();
+    setTimeout(() => {
+      this.setState({
+          refreshing: true
+        },
+        () => {
+          this.getListData(
+            (data) => {
+              if (data.success && data.backData) {
+                const content = data.backData.content ? data.backData.content : [];
+                let listData = this.genData(content);
+                this.setState({
+                  dataSource: dataSource.cloneWithRows(listData),
+                  listData: content,
+                  totalPages: data.backData.totalPages,
+                  hasMore: this.state.pageIndex < data.backData.totalPages - 1
+                });
+              } else {
+                Toast.info(data.backMsg);
+              }
+              this.setState({
+                refreshing: false,
+                initLoaded: true
+              });
+            }
+          );
+        }
+      );
+    }, 100);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {dataSource} = this.state;
+    if (('pageUrl' in nextProps && isEqual(this.props.pageUrl, nextProps.pageUrl) === false)
+      || ('params' in nextProps && isEqual(this.props.params, nextProps.params) === false)) {
+      this.setState({
+          dataSource: dataSource.cloneWithRows({}),
+          listData: [],
+          refreshing: true,
+          isLoading: true,
+          pageIndex: 0,
+          params: nextProps.params
+        },
+        () => {
+          ReactDOM.findDOMNode(this.lv).scrollTop = 0;
+          this.getListData(
+            (data) => {
+              if (data.success && data.backData) {
+                const content = data.backData.content ? data.backData.content : [];
+                this.rData = this.genData(content);
+                this.setState({
+                  dataSource: dataSource.cloneWithRows(this.rData),
+                  listData: content,
+                  totalPages: data.backData.totalPages,
+                  hasMore: this.state.pageIndex < data.backData.totalPages - 1
+                });
+              } else {
+                Toast.info(data.backMsg);
+                this.rData = {};
+                this.setState({
+                  dataSource: dataSource.cloneWithRows(this.rData),
+                  listData: [],
+                  totalPages: 0,
+                  hasMore: false
+                });
+              }
+              this.setState({
+                refreshing: false,
+                isLoading: false,
+              });
+            }
+          );
+        });
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.settingHeight);
+  }
+
+  settingHeight = () => {
+    const clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
+    let hei;
+    const _domNode = ReactDOM.findDOMNode(this.lv);
+    if (this.props.multi) {
+      const tabsHeight = ReactDOM.findDOMNode(this.lv).parentNode.parentNode.parentNode.offsetTop;
+      const offsetTabsHeight = ReactDOM.findDOMNode(this.lv).parentNode.parentNode.parentNode.parentNode.offsetTop;
+      hei = clientHeight - tabsHeight - offsetTabsHeight;
+    } else {
+      if (_domNode.offsetParent) {
+        if (_domNode.offsetParent.offsetParent) {
+          hei = clientHeight - _domNode.offsetTop - _domNode.offsetParent.offsetParent.parentNode.offsetTop;
+        } else {
+          hei = clientHeight - _domNode.offsetTop - _domNode.offsetParent.parentNode.offsetTop;
+        }
+      } else {
+        hei = clientHeight - _domNode.offsetTop;
+      }
+    }
+
+    this.setState({height: hei});
+
+  }
+
+  genData(data) {
+    let dataBlob = {};
+    for (let i = 0; i < data.length; i++) {
+      dataBlob[`${i}`] = data[i];
+    }
+    return dataBlob;
+  }
+
+  onRefresh = () => {
+    const {dataSource} = this.state;
+    this.setState(
+      {
+        refreshing: true,
+        isLoading: true,
+        pageIndex: 0
+      },
+      () => {
+        this.getListData(data => {
+          const content = data.backData.content ? data.backData.content : [];
+          this.rData = this.genData(content);
+          this.setState({
+            dataSource: dataSource.cloneWithRows(this.rData),
             refreshing: false,
             isLoading: false,
-            listData: [],
-            pageIndex: 0,
-            params: {},
-            height: document.documentElement.clientHeight || document.body.clientHeight
-        };
-    }
-
-    componentWillMount() {
-        this.setState({
-            params: this.props.params
+            listData: content,
+            totalPages: data.backData.totalPages,
+            hasMore: this.state.pageIndex < data.backData.totalPages - 1
+          });
         });
+      });
+  }
 
-        window.addEventListener('resize', this.settingHeight);
+  onEndReached = event => {
+    const {hasMore} = this.state;
+    if (!hasMore) {
+      return;
     }
+    this.setState(
+      {
+        isLoading: true,
+        pageIndex: ++this.state.pageIndex
+      }, () => {
+        this.getListData(data => {
+          const {listData, pageIndex, totalPages} = this.state;
+          const content = listData.concat(data.backData.content ? data.backData.content : []);
+          this.rData = this.genData(content);
+          let dataSource = this.state.dataSource.cloneWithRows(this.rData);
+          this.setState({
+            dataSource: dataSource,
+            isLoading: false,
+            listData: content,
+            hasMore: pageIndex < totalPages - 1
+          });
+        });
+      }
+    );
+  }
 
-    componentDidMount() {
-        const {dataSource} = this.state;
-        this.settingHeight();
-        setTimeout(() => {
-            this.setState({
-                    refreshing: true
-                },
-                () => {
-                    this.getListData(
-                        (data) => {
-                            if (data.success && data.backData) {
-                                const content = data.backData.content ? data.backData.content : [];
-                                let listData = this.genData(content);
-                                this.setState({
-                                    dataSource: dataSource.cloneWithRows(listData),
-                                    listData: content,
-                                    totalPages: data.backData.totalPages,
-                                    hasMore: this.state.pageIndex < data.backData.totalPages - 1
-                                });
-                            } else {
-                                Toast.info(data.backMsg);
-                            }
-                            this.setState({
-                                refreshing: false,
-                                initLoaded: true
-                            });
-                        }
-                    );
-                }
-            );
-        }, 100);
-    }
+  getListData = (callback) => {
+    let {params, pageIndex} = this.state;
+    params = assign(params, {pageNumber: ++pageIndex});
+    console.log()
+    axios.post(this.props.pageUrl, params).then(res => res.data).then(data => {
+        if (typeof callback === 'function')
+          callback(data);
+      }
+    );
+  }
 
-    componentWillReceiveProps(nextProps) {
-        const {dataSource} = this.state;
-        if (('pageUrl' in nextProps && isEqual(this.props.pageUrl, nextProps.pageUrl) === false)
-            || ('params' in nextProps && isEqual(this.props.params, nextProps.params) === false)) {
-            this.setState({
-                    dataSource: dataSource.cloneWithRows({}),
-                    listData: [],
-                    refreshing: true,
-                    isLoading: true,
-                    pageIndex: 0,
-                    params: nextProps.params
-                },
-                () => {
-                    ReactDOM.findDOMNode(this.lv).scrollTop = 0;
-                    this.getListData(
-                        (data) => {
-                            if (data.success && data.backData) {
-                                const content = data.backData.content ? data.backData.content : [];
-                                this.rData = this.genData(content);
-                                this.setState({
-                                    dataSource: dataSource.cloneWithRows(this.rData),
-                                    listData: content,
-                                    totalPages: data.backData.totalPages,
-                                    hasMore: this.state.pageIndex < data.backData.totalPages - 1
-                                });
-                            } else {
-                                Toast.info(data.backMsg);
-                                this.rData = {};
-                                this.setState({
-                                    dataSource: dataSource.cloneWithRows(this.rData),
-                                    listData: [],
-                                    totalPages: 0,
-                                    hasMore: false
-                                });
-                            }
-                            this.setState({
-                                refreshing: false,
-                                isLoading: false,
-                            });
-                        }
-                    );
-                });
-        }
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.settingHeight);
-    }
-
-    settingHeight = () => {
-        const clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
-        let hei;
-        const _domNode = ReactDOM.findDOMNode(this.lv);
-        if (this.props.multi) {
-            const tabsHeight = ReactDOM.findDOMNode(this.lv).parentNode.parentNode.parentNode.offsetTop;
-            const offsetTabsHeight = ReactDOM.findDOMNode(this.lv).parentNode.parentNode.parentNode.parentNode.offsetTop;
-            hei = clientHeight - tabsHeight - offsetTabsHeight;
-        } else {
-            if (_domNode.offsetParent) {
-                if (_domNode.offsetParent.offsetParent) {
-                    hei = clientHeight - _domNode.offsetTop - _domNode.offsetParent.offsetParent.parentNode.offsetTop;
-                } else {
-                    hei = clientHeight - _domNode.offsetTop - _domNode.offsetParent.parentNode.offsetTop;
-                }
-            } else {
-                hei = clientHeight - _domNode.offsetTop;
-            }
-        }
-
-        this.setState({height: hei});
-
-    }
-
-    genData(data) {
-        let dataBlob = {};
-        for (let i = 0; i < data.length; i++) {
-            dataBlob[`${i}`] = data[i];
-        }
-        return dataBlob;
-    }
-
-    onRefresh = () => {
-        const {dataSource} = this.state;
-        this.setState(
-            {
-                refreshing: true,
-                isLoading: true,
-                pageIndex: 0
-            },
-            () => {
-                this.getListData(data => {
-                    const content = data.backData.content ? data.backData.content : [];
-                    this.rData = this.genData(content);
-                    this.setState({
-                        dataSource: dataSource.cloneWithRows(this.rData),
-                        refreshing: false,
-                        isLoading: false,
-                        listData: content,
-                        totalPages: data.backData.totalPages,
-                        hasMore: this.state.pageIndex < data.backData.totalPages - 1
-                    });
-                });
-            });
-    }
-
-    onEndReached = event => {
-        const {hasMore} = this.state;
-        if (!hasMore) {
-            return;
-        }
-        this.setState(
-            {
-                isLoading: true,
-                pageIndex: ++this.state.pageIndex
-            }, () => {
-                this.getListData(data => {
-                    const {listData, pageIndex, totalPages} = this.state;
-                    const content = listData.concat(data.backData.content ? data.backData.content : []);
-                    this.rData = this.genData(content);
-                    let dataSource = this.state.dataSource.cloneWithRows(this.rData);
-                    this.setState({
-                        dataSource: dataSource,
-                        isLoading: false,
-                        listData: content,
-                        hasMore: pageIndex < totalPages - 1
-                    });
-                });
-            }
-        );
-    }
-
-    getListData = (callback) => {
-        let {params, pageIndex} = this.state;
-        params = assign(params, {pageNumber: pageIndex});
-
-        axios.get(this.props.pageUrl, params).then(res => res.data).then(data => {
-                if (typeof callback === 'function')
-                    callback(data);
-            }
-        );
-    }
-
-    render() {
-        const {dataSource, initLoaded, refreshing, isLoading} = this.state;
-        const {row} = this.props;
-        return (
-            <ListView
-                ref={el => this.lv = el}
-                dataSource={dataSource}
-                renderFooter={() => (<div style={{padding: 30, textAlign: 'center'}}>
-                    {
-                        initLoaded && !refreshing ? (isLoading ? '正在加载...' : '没有了啦~') : null
-                    }
-                </div>)}
-                renderBodyComponent={() => <MyBody/>}
-                renderRow={row}
-                className="zui-cardlist"
-                style={{
-                    height: this.state.height,
-                    overflow: 'auto',
-                }}
-                pageSize={4}
-                pullToRefresh={<PullToRefresh
-                    distanceToRefresh={30}
-                    refreshing={refreshing}
-                    onRefresh={this.onRefresh}
-                    indicator={{
-                        activate: <div className='loader'>
-                            <div className='loader-inner ball-beat'>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                            </div>
-                        </div>,
-                        deactivate: <div className='loader'>
-                            <div className='loader-inner ball-beat'>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                            </div>
-                        </div>,
-                        release: <div className='loader'>
-                            <div className='loader-inner ball-beat'>
-                                <div></div>
-                                <div></div>
-                                <div></div>
-                            </div>
-                        </div>,
-                        finish: <span>刷新完成</span>
-                    }}
-                />}
-                scrollRenderAheadDistance={500}
-                onEndReached={this.onEndReached}
-                onEndReachedThreshold={500}
-            />
-        );
-    }
+  render() {
+    const {dataSource, initLoaded, refreshing, isLoading} = this.state;
+    const {row} = this.props;
+    return (
+      <ListView
+        ref={el => this.lv = el}
+        dataSource={dataSource}
+        renderFooter={() => (<div style={{padding: 30, textAlign: 'center'}}>
+          {
+            initLoaded && !refreshing ? (isLoading ? '正在加载...' : '没有了啦~') : null
+          }
+        </div>)}
+        renderBodyComponent={() => <MyBody/>}
+        renderRow={row}
+        className="zui-cardlist"
+        style={{
+          height: this.state.height,
+          overflow: 'auto',
+        }}
+        pageSize={4}
+        pullToRefresh={<PullToRefresh
+          distanceToRefresh={30}
+          refreshing={refreshing}
+          onRefresh={this.onRefresh}
+          indicator={{
+            activate: <div className='loader'>
+              <div className='loader-inner ball-beat'>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>,
+            deactivate: <div className='loader'>
+              <div className='loader-inner ball-beat'>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>,
+            release: <div className='loader'>
+              <div className='loader-inner ball-beat'>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>,
+            finish: <span>刷新完成</span>
+          }}
+        />}
+        scrollRenderAheadDistance={500}
+        onEndReached={this.onEndReached}
+        onEndReachedThreshold={500}
+      />
+    );
+  }
 }
 
 Index.defaultProps = {
-    mode: 'block',
+  mode: 'block',
 };
 
 export default Index;
---
+++ src/modules/shop/component/shopDetail.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import {Tabs, Toast, SearchBar} from 'antd-mobile';
+import '../index.less';
+import restUrl from "RestUrl";
+import DocumentTitle from "react-document-title";
+import axios from "Utils/axios";
+import assign from 'lodash/assign';
+
+import {CardList} from 'Comps';
+
+const GoodsCart = ({className = '', data, ...restProps}) => (
+  <div className={`${className} goodsCart`} {...restProps}>
+    <div className='goodsImg'>
+      <img src={data.File ? (restUrl.FILE_ASSET + data.File.id + data.File.fileType) : ''} alt=""/>
+    </div>
+    <div className='goodsBody'>
+      <div className='goodsHeader'>
+        {data.isTop ? <div className='isTop'>精品</div> : null}
+        {data.productName}
+      </div>
+      <div className='goodsContent'>
+        <div className='goodsLabel'>【{data.shopName}】</div>
+        <div className='goodsAddress'>{data.shopAddress}</div>
+      </div>
+      <div className='goodsFooter'>
+        <div><span>￥</span><span style={{fontSize: 15}}>{data.productSellingprice}</span></div>
+      </div>
+    </div>
+  </div>
+);
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+      params: {
+        id: this.props.params.id,
+        pageNumber: 1,
+        pageSize: 10,
+      },
+      keyWords: '',
+      shopDetail: {},
+      goodsList: []
+    }
+  };
+
+  componentDidMount() {
+    this.queryShopDetail();
+  }
+
+  queryShopDetail = () => {
+    const param = {
+      id: this.props.params.id
+    };
+    axios.get('shop/queryDetail', {
+      params: param
+    }).then(res => res.data).then(data => {
+      if (data.success) {
+        if (data.backData) {
+          let backData = data.backData;
+          let shopPic = backData.shopPic;
+          shopPic.map(item => {
+            item.imgSrc = restUrl.FILE_ASSET + `${item.id + item.fileType}`;
+          });
+          backData.shopPic = shopPic[0];
+          this.setState({
+            shopDetail: backData
+          });
+        }
+      }
+    });
+  }
+
+  detail = (id) => {
+    this.context.router.push(`/food/detail/${id}`);
+  }
+
+  render() {
+    const {params, shopDetail} = this.state;
+    const row = (rowData, sectionID, rowID) => {
+      const obj = rowData;
+
+      return (
+        <div key={rowID} style={{float: 'left', width: '50%'}}>
+          <GoodsCart key={obj.id} data={obj} onClick={() => this.detail(obj.id)}/>
+        </div>
+      );
+    }
+
+    return (
+      <DocumentTitle title='商品小铺'>
+        <div className="shop">
+          <div className="zui-content">
+            <SearchBar placeholder="请输入要搜索的商铺" maxLength={16}/>
+            <div className="goods-store">
+              <div className='goods-store-header'>
+                <div className='goods-store-logo'>
+                  {
+                    shopDetail.shopPic && shopDetail.shopPic.imgSrc ?
+                      (
+                        <img
+                          src={shopDetail.shopPic && shopDetail.shopPic.imgSrc}
+                          alt=""/>
+                      ) : (
+                        shopDetail.shopName && shopDetail.shopName.slice(0, 2)
+                      )
+                  }
+                </div>
+                <div className='goods-store-info'>
+                  {shopDetail.shopName}
+                </div>
+                <div className='goods-store-into'>
+                  收藏店铺
+                </div>
+              </div>
+              <div className='goods-store-body'>
+                {shopDetail.mark}
+              </div>
+            </div>
+            <CardList
+              pageUrl={'product/queryListByShopId'}
+              params={params}
+              row={row}
+            />
+          </div>
+        </div>
+      </DocumentTitle>
+    );
+  }
+}
+
+Index.contextTypes = {
+  router: PropTypes.object
+}
+
+export default Index;
